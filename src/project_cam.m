f511a3616b03d0d1f66ff9f67790fc9711f8426b
Dimitris Papadimitriou <jim_pap@live.com>
2021-04-16 20:31:37 +0300

    ata

--- src/project_cam.m
+++ src/project_cam.m
@@ -2,10 +2,7 @@ function [P, D] = project_cam(w, cv, cx, cy, cz, p)
     %p: 3x1 point (3xN)
     %cv: 3x1 cam center
     %cx, cy, cz: 3x1 cam axis
-    %w: apostasi petasmatos
-    P = zeros(2,size(p,2));
-    D = zeros(size(p,2));
-    
+    %w: apostasi petasmatos   
     cam = transformation_matrix;
     cam.T(1:3,1:4)=[cx cy cz cv];
     invcam
